4083d298a2445c8c3b01e77e30c47a22db6632cd
Xetacon <alexj.godwin@gmail.com>
2021-03-28 14:09:10 -0400

    notation

--- super-disco-master/Develop/script.js
+++ super-disco-master/Develop/script.js
@@ -1,8 +1,8 @@
-// set the date at the top of the page
+// set date
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-// tasks object to store in localStorage.
+// tasks to store in localStorage.
 var tasks = {
     "9": [],
     "10": [],
@@ -21,7 +21,7 @@ var setTasks = function() {
 }
 
 var getTasks = function() {
-    /* load the tasks from localStorage and create tasks in the right row */
+    /* load the tasks from localStorage and create tasks in correct row */
 
     var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
     if (loadedTasks) {
@@ -34,7 +34,7 @@ var getTasks = function() {
         })
     }
 
-    // make sure the past/current/future time is reflected
+    // make sure the time is reflected
     auditTasks()
 }
 
@@ -55,7 +55,7 @@ var auditTasks = function() {
     $(".task-info").each( function() {
         var elementHour = parseInt($(this).attr("id"));
 
-        // handle past, present, and future
+        
         if ( elementHour < currentHour ) {
             $(this).removeClass(["present", "future"]).addClass("past");
         }
@@ -69,56 +69,44 @@ var auditTasks = function() {
 };
 
 var replaceTextarea = function(textareaElement) {
-    /* replaces the provided textarea element with a p element and persists the data in localStorage */
-
-    // get the necessary elements
     var taskInfo = textareaElement.closest(".task-info");
     var textArea = taskInfo.find("textarea");
 
-    // get the time and task
     var time = taskInfo.attr("id");
     var text = textArea.val().trim();
 
-    // persist the data
-    tasks[time] = [text];  // setting to a one item list since there's only one task for now
+    tasks[time] = [text]; 
     setTasks();
 
-    // replace the textarea element with a p element
     createTask(text, taskInfo);
 }
 
 /* CLICK HANDLERS */
 
-// tasks
 $(".task").click(function() {
 
-    // save the other tasks if they've already been clicked
     $("textarea").each(function() {
         replaceTextarea($(this));
     })
 
-    // convert to a textarea element if the time hasn't passed
     var time = $(this).closest(".task-info").attr("id");
     if (parseInt(time) >= moment().hour()) {
 
-        // create a textInput element that includes the current task
         var text = $(this).text();
         var textInput = $("<textarea>")
             .addClass("form-control")
             .val(text);
 
-        // add the textInput element to the parent div
         $(this).html(textInput);
         textInput.trigger("focus");
     }
 })
 
-// save button click handler
+// save button click 
 $(".saveBtn").click(function() {
     replaceTextarea($(this));
 })
 
-// update task backgrounds on the hour
 timeToHour = 3600000 - today.milliseconds();  // check how much time is left until the next hour
 setTimeout(function() {
     setInterval(auditTasks, 3600000)
